5090086d74935ad5cc501c0ed8693339390059bd
Finn Baumann <hello@baumannfinn.de>
2022-11-13 12:23:58 +0100

    Removed password query in registration

--- app.py
+++ app.py
@@ -150,14 +150,8 @@ def register():
         password = request.form['password']
         db = connect_db()
         c = db.cursor()
-        pass_statement = """SELECT * FROM users WHERE password = ?;"""
+
         user_statement = """SELECT * FROM users WHERE username = ?;"""
-        # NOT WORKING:
-        # 'or username = 'a
-        c.execute(pass_statement, password)
-        if(len(c.fetchall())>0):
-            errored = True
-            passworderror = "That password is already in use by someone else!"
 
         c.execute(user_statement, username)
         if(len(c.fetchall())>0):
