73068d99be003d8cdecfdec907a9594f6ad2c343
Babavaraprasad <bapi@itu.dk>
2022-11-14 20:04:26 +0100

    sql

--- app.py
+++ app.py
@@ -104,15 +104,15 @@ def notes():
             db.close()
 
         elif request.form['submit_button'] == 'update note':
-             noteupdate = request.form['noteupdate']
-             db = connect_db()
-             c = db.cursor()
-             statement = """update notes set note='%s' where assocUser='%s';""" %(noteupdate, session['userid'])
-             print(statement)
-             c.execute(statement)
-             db.commit()
-             db.close()
-
+            noteupdate = request.form['noteupdate']
+            noteupdateid = request.form['noteupdateid']
+            db = connect_db()
+            c = db.cursor()
+            statement = """update notes set note='%s' where publicID='%s';""" %(noteupdate, noteupdateid)
+            print(statement)
+            c.execute(statement)
+            db.commit()
+            db.close()
     db = connect_db()
     c = db.cursor()
     statement = "SELECT * FROM notes WHERE assocUser = ?;"
